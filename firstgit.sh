9c9a9956c9fb5dcbb7e542ed3402360901732803
dabaicai <dabaicai@gmail.com>
2020-09-25 15:40:41 +0800

    ok

--- firstgit.sh
+++ firstgit.sh
@@ -2,9 +2,10 @@ mv ./libraries/appbase/.git    ./libraries/appbase/.gitbak
 mv ./libraries/chainbase/.git  ./libraries/chainbase/.gitbak
 mv ./libraries/fc/.git         ./libraries/fc/.gitbak
 git add .
-git commit -m "$0"
+git commit -m $1
 git push origin master
 
 mv ./libraries/appbase/.gitbak    ./libraries/appbase/.git
 mv ./libraries/chainbase/.gitbak  ./libraries/chainbase/.git
 mv ./libraries/fc/.gitbak         ./libraries/fc/.git
+#git remote set-url origin git@github.com:onetwodays/eos.git
